954f55b0461247dc04680a46661e678a51532b76
seejn <seejnmaharjan@gmail.com>
2024-09-06 10:24:23 +0545

    created fetchOptions method to return options layout
    created handleSetId method to disable select and buy button when value "null" selected

--- src/containers/bookmovie/Book.jsx
+++ src/containers/bookmovie/Book.jsx
@@ -1,7 +1,93 @@
+import { useState, useEffect, useContext } from "react";
+import CityContext from "../../context/CityContext";
+import { getCinemas, getMovies, getDates, getTime } from "@apis/booking";
+import BookingContext from "../../context/BookingContext";
+
 export default function () {
+    const [cinemasDropdown, setCinemasDropdown] = useState([]);
+    const [moviesDropdown, setMoviesDropdown] = useState([]);
+    const [dateDropdown, setDateDropdown] = useState([]);
+    const [timeDropdown, setTimeDropdown] = useState([]);
+
+    const { cityId } = useContext(CityContext);
+
+    const citySelectionWarning = !cityId && (
+        <div className="my-2 text-center  text-red-800 font-bold">City not selected</div>
+    );
+
+    const {
+        cinemaId,
+        setCinemaId,
+        movieId,
+        setMovieId,
+        dateId,
+        setDateId,
+        timeId,
+        setTimeId,
+    } = useContext(BookingContext);
+
+    const fetchOptions = (fetchFn, setDropdown, dependencies = []) => {
+        const data = dependencies.every(Boolean) ? fetchFn(...dependencies) : [];
+        console.log("data", data);
+        const options = data?.map(d => (
+            <option key={d.id} value={d.id}>
+                {d.name || d.slot || d.date}
+            </option>
+        ));
+        setDropdown(options)
+    }    
+
+    const handleSetId = (setFunc, selectId, selectedValue) => {
+        setFunc(selectedValue)
+        if(!selectedValue){
+            switch(selectId){
+                case "cinema":
+                    setMovieId(null)
+                case "movie":
+                    setDateId(null)
+                case "date":
+                    setTimeId(null)
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+
+    useEffect(() => {
+        fetchOptions(getCinemas, setCinemasDropdown, [cityId]);
+    }, [cityId]);
+
+    useEffect(() => {
+        fetchOptions(getMovies, setMoviesDropdown, [cinemaId])
+        
+    }, [cinemaId]);
+
+    useEffect(() => {
+        fetchOptions(getDates, setDateDropdown, [cinemaId, movieId])
+    }, [cinemaId, movieId]);
+
+    useEffect(() => {
+        fetchOptions(getTime, setTimeDropdown, [cinemaId, movieId, dateId])
+    }, [cinemaId, movieId, dateId]);
+
+    const buyButton = (
+        !cityId ||
+        !cinemaId ||
+        !movieId ||
+        !dateId ||
+        !timeId
+    ) ? null:(
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded sm:self-stretch">
+            Buy
+        </button>
+    );
+
+
     return (
         <>
             <div className="sticky w-full bottom-0 bg-white shadow-md rounded-lg p-4 sm:p-6 mx-auto mt-8">
+                { citySelectionWarning }
                 <div className="flex flex-col sm:flex-row sm:items-end sm:space-x-4 space-y-4 sm:space-y-0">
                     <div className="flex-1 min-w-0">
                         <label
@@ -13,11 +99,11 @@ export default function () {
                         <select
                             id="cinema"
                             className="w-full bg-gray-100 border border-gray-300 text-gray-700 py-2 px-3 rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
+                            onChange={(e) => handleSetId(setCinemaId, "cinema", parseInt(e.target.value))}
+                            disabled={!cityId}
                         >
                             <option>Select a Cinema</option>
-                            <option>Cinema 1</option>
-                            <option>Cinema 2</option>
-                            <option>Cinema 3</option>
+                            {cinemasDropdown}
                         </select>
                     </div>
 
@@ -31,11 +117,11 @@ export default function () {
                         <select
                             id="movie"
                             className="w-full bg-gray-100 border border-gray-300 text-gray-700 py-2 px-3 rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
+                            onChange={(e) => handleSetId(setMovieId, "movie", parseInt(e.target.value))}
+                            disabled={!cinemaId}
                         >
                             <option>Select a Movie</option>
-                            <option>Movie 1</option>
-                            <option>Movie 2</option>
-                            <option>Movie 3</option>
+                            {moviesDropdown}
                         </select>
                     </div>
 
@@ -46,11 +132,15 @@ export default function () {
                         >
                             Select Date
                         </label>
-                        <input
-                            type="date"
+                        <select
                             id="date"
                             className="w-full bg-gray-100 border border-gray-300 text-gray-700 py-2 px-3 rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
-                        />
+                            onChange={(e) => handleSetId(setDateId, "date", parseInt(e.target.value))}
+                            disabled={!cinemaId || !movieId}
+                        >
+                            <option>Select a Date</option>
+                            {dateDropdown}
+                        </select>
                     </div>
 
                     <div className="flex-1 min-w-0">
@@ -63,18 +153,14 @@ export default function () {
                         <select
                             id="time"
                             className="w-full bg-gray-100 border border-gray-300 text-gray-700 py-2 px-3 rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
+                            onChange={(e) => handleSetId(setTimeId, "time", parseInt(e.target.value))}
+                            disabled={!cinemaId || !movieId || !dateId}
                         >
                             <option>Select a Time</option>
-                            <option>10:00 AM</option>
-                            <option>1:00 PM</option>
-                            <option>4:00 PM</option>
-                            <option>7:00 PM</option>
-                        </select>
+                            {timeDropdown}
+                            </select>
                     </div>
-
-                    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded sm:self-stretch">
-                        Buy
-                    </button>
+                   { buyButton }
                 </div>
             </div>
         </>
