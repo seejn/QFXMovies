5e7b245a3d9a215e27abf12dcfe10922074bd5e8
seejn <seejnmaharjan@gmail.com>
2024-11-11 21:47:03 +0545

    added apis, data, pages for easy path import

--- vite.config.js
+++ vite.config.js
@@ -7,9 +7,12 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
+      "@apis": path.resolve(__dirname, 'src/apis'),
       "@data": path.resolve(__dirname, 'src/data'),
       "@components": path.resolve(__dirname, 'src/components'),
       "@containers": path.resolve(__dirname, 'src/containers'),
+      "@context": path.resolve(__dirname, 'src/context'),
+      "@pages": path.resolve(__dirname, 'src/pages'),
     },
   },
 });
