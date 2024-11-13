2012e9e6029a2d6863c9a527c986cbbd454da78f
seejn <seejnmaharjan@gmail.com>
2024-11-13 22:02:08 +0545

    created method for handling delete and update api request

--- src/apis/cities.js
+++ src/apis/cities.js
@@ -20,11 +20,45 @@ export const addCity = async (data) => {
             method: "POST",
             data: data,
             headers: {
-                Authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY3MzIyMmFkNDM4MmQ0ZWE2MWRiZjI5ZCIsImVtYWlsIjoic2Vlam5tYWhhcmphbkBnbWFpbC5jb20iLCJyb2xlIjoiYWRtaW4iLCJpYXQiOjE3MzEzNDAxNDYsImV4cCI6MTczMTM0Mzc0Nn0.wjVdxt_BEV4FeQ0NfkB5u8nhp0jEdyBHoy40FNAdG8E"
+                Authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY3MzIyMmFkNDM4MmQ0ZWE2MWRiZjI5ZCIsImVtYWlsIjoic2Vlam5tYWhhcmphbkBnbWFpbC5jb20iLCJyb2xlIjoiYWRtaW4iLCJpYXQiOjE3MzE1MTM4NTEsImV4cCI6MTczMTUxNzQ1MX0.dEwUOVYxU0n50j_ja64l4Bxut0nURcKN-iCAiYDkJhk"
             }
         });
         alert(response.data.message);
         return response.data.data;
+    }catch(error){
+        console.log(error);
+        throw error;
+    }
+}
+
+export const updateCity = async (id, data) => {
+    try{
+        const response = await axios({
+            url: `${baseUrl}/cities/${id}`,
+            method: "PATCH",
+            data: data,
+            headers: {
+                Authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY3MzIyMmFkNDM4MmQ0ZWE2MWRiZjI5ZCIsImVtYWlsIjoic2Vlam5tYWhhcmphbkBnbWFpbC5jb20iLCJyb2xlIjoiYWRtaW4iLCJpYXQiOjE3MzE1MTM4NTEsImV4cCI6MTczMTUxNzQ1MX0.dEwUOVYxU0n50j_ja64l4Bxut0nURcKN-iCAiYDkJhk"
+            }
+        });
+        alert(response.data.message);
+        return response.data.data;
+    }catch(error){
+        console.log(error);
+        throw error;
+    }
+}
+
+export const deleteCity = async (id) => {
+    try{
+        const response = await axios({
+            url: `${baseUrl}/cities/${id}`,
+            method: "DELETE",
+            headers: {
+                Authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY3MzIyMmFkNDM4MmQ0ZWE2MWRiZjI5ZCIsImVtYWlsIjoic2Vlam5tYWhhcmphbkBnbWFpbC5jb20iLCJyb2xlIjoiYWRtaW4iLCJpYXQiOjE3MzE1MTM4NTEsImV4cCI6MTczMTUxNzQ1MX0.dEwUOVYxU0n50j_ja64l4Bxut0nURcKN-iCAiYDkJhk"
+            }
+        });
+        alert(response.data.message);
     }catch(error){
         console.log(error);
         throw error;
